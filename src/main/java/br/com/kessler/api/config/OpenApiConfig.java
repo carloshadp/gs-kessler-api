16259b18e741c046546d91a005b88d1bf544ad46
Carlos Henrique Alves <carloshadp@gmail.com>
2026-06-09 20:46:05 -0300

    (chore): ajuste swagger

--- src/main/java/br/com/kessler/api/config/OpenApiConfig.java
+++ src/main/java/br/com/kessler/api/config/OpenApiConfig.java
@@ -3,11 +3,8 @@ package br.com.kessler.api.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -22,7 +19,6 @@ public class OpenApiConfig {
                         .version("1.0.0")
                         .contact(new Contact()
                                 .name("Equipe Kessler")
-                                .email("kessler@fiap.com.br")))
-                .servers(List.of(new Server().url("http://localhost:8080").description("Local")));
+                                .email("kessler@fiap.com.br")));
     }
 }
